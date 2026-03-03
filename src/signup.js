6037adae48ed3ded988c187e26b40106a263b5c4
Halie Favron <halieanastasia@gmail.com>
2026-03-03 14:10:43 -0800

    Updated sign up page

--- src/signup.js
+++ src/signup.js
@@ -1,25 +1,24 @@
+const openBtn = document.getElementById("open-modal-btn");
+const closeBtn = document.getElementById("close-modal-btn");
+const dialog = document.getElementById("signup-dialog");
 
-const openBtn = document.getElementById('open-modal-btn');
-const closeBtn = document.getElementById('close-modal-btn');
-const dialog = document.getElementById('signup-dialog');
-
-openBtn.addEventListener('click', () => {
-    dialog.showModal();
+openBtn.addEventListener("click", () => {
+  dialog.showModal();
 });
 
-closeBtn.addEventListener('click', () => {
-    dialog.close();
+closeBtn.addEventListener("click", () => {
+  dialog.close();
 });
 
-const form = document.getElementById('signup-form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    console.log('Form submitted!');
-    dialog.close();
+const form = document.getElementById("signup-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Form submitted!");
+  dialog.close();
 });
-const redirectBtn = document.getElementById('signup-redirect-btn');
+const redirectBtn = document.getElementById("signup-redirect-btn");
 
-redirectBtn.addEventListener('click', () => {
-    // window.location.href changes the current URL of the browser
-    window.location.href = 'signup.html';
+redirectBtn.addEventListener("click", () => {
+  // window.location.href changes the current URL of the browser
+  window.location.href = "signup.html";
 });
