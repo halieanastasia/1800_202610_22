78b5148f4ea60dcc1c358628352398c0d41b7b26
Suzy Hham <suzyhham02@gmail.com>
2026-03-09 07:08:31 -0700

    Navbar links and Firestore restaurant data working

--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "../styles/style.css";
-import { collection, getDocs, addDoc, serverTimestamp } from "firebase/firestore";
-import { seedRestaurants } from './restaurants.js';
+import { seedRestaurants } from "./restaurants.js";
 import "./navbar.js";
-
-function sayHello() { }
-// document.addEventListener('DOMContentLoaded', sayHello);
-
-
-
-
-
 
 seedRestaurants();
